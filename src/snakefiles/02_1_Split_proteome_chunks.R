46cb5ff080d794ef2c6ef5a35bdbd4d65b2eeb73
Yehor Horokhovskyi <y.horokhovskyi@gmail.com>
2021-11-12 16:24:05 +0100

    Now compatible with Uniprot headers

--- src/snakefiles/02_1_Split_proteome_chunks.R
+++ src/snakefiles/02_1_Split_proteome_chunks.R
@@ -15,41 +15,42 @@ sink(log)
 
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(seqinr))
+suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(stringi))
 suppressPackageStartupMessages(library(parallel))
 suppressPackageStartupMessages(library(parallelly))
 suppressPackageStartupMessages(library(foreach))
-
-{
-  # # Manual startup
-  # source("src/snakefiles/functions.R")
-  # 
-  # proteome <- "data/reference/Measles_CDS_6_frame.fasta"
-  # dat = read.fasta(file=proteome, seqtype="AA", as.string = TRUE)
-  # proteome <- unlist(strsplit(proteome, "/", fixed = T))[grep(".fasta", unlist(strsplit(proteome, "/", fixed = T)))]
-  # proteome <- unlist(strsplit(proteome, ".fasta", fixed = T))[1]
-  # 
-  # prot_cluster <- read.table("results/Cluster/Measles_CDS_6_frame/Measles_CDS_6_frame_cluster.tsv")
-  # 
-  # Master_table <- read.csv("Master_table.csv") %>%
-  #   as_tibble()  %>%
-  #   filter(Proteome == proteome) %>%
-  #   select(Proteome, MaxE, Min_Interv_length)
-  # 
-  # # max intervening sequence length
-  # MiSl <- 25
-  # min_protein_length = 8
-  # dat <- dat[which(lapply(dat, nchar) >= min_protein_length)]
-  # 
-  # max_length=500
-  # overlap_length=MiSl*2
-  # maxE = Master_table$MaxE
-  # replace_I_with_L = TRUE
-  # 
-  # Ncpu = availableCores()
-  # cl <- parallel::makeForkCluster(Ncpu)
-  # cl <- parallelly::autoStopCluster(cl)
-}
+suppressPackageStartupMessages(library(vroom))
+# {
+#   # Manual startup
+#   source("src/snakefiles/functions.R")
+# 
+#   proteome <- "data/reference/Measles_CDS_6_frame.fasta"
+#   dat = read.fasta(file=proteome, seqtype="AA", as.string = TRUE)
+#   proteome <- unlist(strsplit(proteome, "/", fixed = T))[grep(".fasta", unlist(strsplit(proteome, "/", fixed = T)))]
+#   proteome <- unlist(strsplit(proteome, ".fasta", fixed = T))[1]
+# 
+#   prot_cluster <- vroom("results/Cluster/Measles_CDS_6_frame/Measles_CDS_6_frame_cluster.tsv", col_names = c("V1", "V2"), delim = "\t")
+# 
+#   Master_table <- read.csv("Master_table.csv") %>%
+#     as_tibble()  %>%
+#     filter(Proteome == proteome) %>%
+#     select(Proteome, MaxE, Min_Interv_length)
+# 
+#   # max intervening sequence length
+#   MiSl <- 25
+#   min_protein_length = 8
+#   dat <- dat[which(lapply(dat, nchar) >= min_protein_length)]
+# 
+#   max_length=500
+#   overlap_length=MiSl*2
+#   maxE = Master_table$MaxE
+#   replace_I_with_L = FALSE
+# 
+#   Ncpu = availableCores()
+#   cl <- parallel::makeForkCluster(Ncpu)
+#   cl <- parallelly::autoStopCluster(cl)
+# }
 
 source(snakemake@input[["functions"]])
 print("Loaded functions. Loading the data")
@@ -65,7 +66,7 @@ proteome <- unlist(strsplit(proteome, "/", fixed = T))[grep(".fasta", unlist(str
 proteome <- unlist(strsplit(proteome, ".fasta", fixed = T))[1]
 
 # Clustering
-prot_cluster <- read.table(snakemake@input[["prot_cluster"]])
+prot_cluster <- vroom(snakemake@input[["prot_cluster"]], col_names = c("V1", "V2"), delim = "\t", show_col_types = FALSE)
 
 # Master table
 Master_table <- read.csv("Master_table.csv") %>%
@@ -113,6 +114,13 @@ suppressWarnings(
 ### ---------------------------- (2) Proteome pre-processing --------------------------------------
 # Filter by minimal length and re-order by similarity from clustering
 dat <- dat[which(lapply(dat, nchar) >= min_protein_length)]
+
+# Extract Uniprot headers like in mmseqs2
+if (FALSE %in% (names(dat) %in% prot_cluster$V2)) {
+  names(dat) <- ifelse(stri_detect_regex(names(dat), pattern = "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"),
+                       stri_extract(names(dat), regex = "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"),
+                       names(dat))
+}
 prot_cluster <- prot_cluster[prot_cluster$V2 %in% names(dat),]
 dat <- dat[match(prot_cluster$V2, names(dat))]
 
@@ -129,8 +137,6 @@ if (replace_I_with_L == TRUE) {
 dat <- Split_list_max_length_parallel(String_list = dat, 
                                       max_length=max_length, 
                                       overlap_length=unique(overlap_length))
-
-
 # Estimate chunks
 for (i in 1:nrow(Master_table)) {
   maxE <- Master_table$MaxE[i]
